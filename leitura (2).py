a8eb403be127c69ef23878035a1d6e612869e847
sabinodavid <sabinodavid2007@gmail.com>
2025-06-07 14:33:00 -0300

    Add files via upload

---
+++ leitura (2).py
@@ -0,0 +1,12 @@
+def main():
+  nomeEscola = input("Digite o nome da escola: ")
+  idade = int(input("Digite a sua idade: "))
+  tamanho = float(input("Digite o seu tamanho: "))
+  sexo = input("Digite o seu sexo: (M/F/N) ")
+
+  print(nomeEscola)
+  print(idade)
+  print(tamanho)
+  print(sexo)
+  return 0
+main()
